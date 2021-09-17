6d577f134c7b93eb0c5afecf233a001b848b6443
Saloni <salonimehta2127@gmail.com>
2021-09-16 22:36:48 -0400

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('MR. Chocolate',30,'M','brown','joyful',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',15,'M','white','empathetic',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',10,'F','brown','playful',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',12,'F','white','joyful',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',10,'F','brown','fearful',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',25,'M','brown','joyful',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',13,'F','brown','joyful',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(null,25,'M','brown','playful',1);
